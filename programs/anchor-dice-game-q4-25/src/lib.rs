c78fb6f89f3525fa25bb7327fdd47017e8ccab16
Anurag Munda <anurag.munda005@gmail.com>
2026-02-11 09:39:32 +0000

    program id update

--- programs/anchor-dice-game-q4-25/src/lib.rs
+++ programs/anchor-dice-game-q4-25/src/lib.rs
@@ -7,7 +7,7 @@ use anchor_lang::prelude::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("DZDRzKdTu4SweFFjDutMgPqu55Qt9TLbhWG1cMAikYVp");
+declare_id!("HVzUwdbxc6NkULKHyFDR6BCryqB6beFEZWGKVNkPkQ76");
 
 #[program]
 pub mod anchor_dice_game_q4_25 {
